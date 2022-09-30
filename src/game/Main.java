607a4d79162b0ebe2ff4a6662d46bc9e344c0b7f
Evan Harrigan <54324630+harriganevan@users.noreply.github.com>
2022-09-30 02:04:47 -0400

    Add files via upload

---
+++ src/game/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Evan Harrigan
+ * CSA
+ * CMDR Schenk
+ * Master Project
+ * 4/24/2020
+ */
+
+package harrigan.five;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import game.GamePanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame gameFrame = new JFrame("Corona Defense");
+				gameFrame.add(new GamePanel());
+				gameFrame.setResizable(false);
+				gameFrame.pack();
+				gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				gameFrame.setLocationRelativeTo(null);
+				gameFrame.setVisible(true);
+			}
+		});
+	}
+}
